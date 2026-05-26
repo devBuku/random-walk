2da43e772583d4e7588d2279bae80478a5a2d514
devBuku <shubhayanbagchi30@gmail.com>
2026-05-26 22:06:35 +0530

    dotted rectangles

--- code.c
+++ code.c
@@ -4,9 +4,32 @@
 #include <SDL2/SDL_video.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define WIDTH 900
 #define HEIGHT 600
+
+#define SCALE 10
+
+typedef struct {
+  int vx, vy;
+} Velocity;
+
+Velocity get_rand_v() {
+  int choice = (rand() / (RAND_MAX / 4));
+  switch (choice) {
+  case 0: // up
+    return (Velocity){0, -1};
+  case 1: // down
+    return (Velocity){0, 1};
+  case 2: // left
+    return (Velocity){-1, 0};
+  case 3: // right
+    return (Velocity){1, 0};
+  }
+  fprintf(stderr, "Impossible random value %d encountered\n", choice);
+  exit(-1);
+}
 
 int main(int argc, const char *argv[]) {
   int num_agents;
@@ -16,7 +39,10 @@ int main(int argc, const char *argv[]) {
     num_agents = atoi(argv[1]);
   } else {
     printf("Usage: %s <num-of-agents>\n", argv[0]);
+    return -1;
   }
+
+  srand(time(NULL));
 
   SDL_Window *pwindow =
       SDL_CreateWindow("Random Walk", SDL_WINDOWPOS_CENTERED,
@@ -24,9 +50,7 @@ int main(int argc, const char *argv[]) {
 
   SDL_Surface *psurface = SDL_GetWindowSurface(pwindow);
 
-  SDL_Rect rect = (SDL_Rect){50, 50, 50, 50};
-  SDL_FillRect(psurface, &rect, 0xFFFFFF);
-  SDL_UpdateWindowSurface(pwindow);
+  SDL_Rect rect = (SDL_Rect){50, 50, 2, 2};
 
   int app_running = 1;
   while (app_running) {
@@ -36,5 +60,14 @@ int main(int argc, const char *argv[]) {
         app_running = 0;
       }
     }
+
+    Velocity v = get_rand_v();
+    rect.x += v.vx * SCALE;
+    rect.y += v.vy * SCALE;
+
+    SDL_FillRect(psurface, &rect, 0xFFFFFF);
+    SDL_UpdateWindowSurface(pwindow);
+
+    SDL_Delay(20);
   }
 }
